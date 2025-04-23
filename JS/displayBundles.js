32e91421715329ebaf054cf4201fd31dd1047a40
Matheus Coelho <matheusvgp29@gmail.com>
2025-04-22 23:58:48 -0300

    Atualiza exibição de bundles: corrige nomes e preços, adiciona log do total de bundles

--- JS/displayBundles.js
+++ JS/displayBundles.js
@@ -1,21 +1,26 @@
 function displayBundles(data) {
   const bundlesDiv = document.getElementById("bundles");
+
   data.bundles.forEach((bundle) => {
     const bundleDiv = document.createElement("div");
     bundleDiv.classList.add("card");
     bundleDiv.innerHTML = `
       <img src="${bundle.header_image}" alt="${bundle.Nome}">
       <div class="card-content">
-        <h3>${bundle.Nome}</h3>
+        <h3>${bundle.name}</h3>
         <p>${bundle.description}</p>
         <div class="preco-btn-container">
-          <p class="preco">R$ ${bundle.cost}</p>
+          <p class="preco">${bundle.formatted_final_price}</p>
           <a href="${bundle.Link}" target="_blank" rel="noopener noreferrer">Ver na Steam</a>
         </div>
       </div>
     `;
     bundlesDiv.appendChild(bundleDiv);
   });
+
+  // Exibe o total de bundles, se necessário
+  const totalBundles = data.totalBundles;
+  console.log(`Total de bundles disponíveis: ${totalBundles}`);
 }
 
 export { displayBundles };
