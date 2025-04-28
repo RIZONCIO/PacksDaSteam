f195c6da85a131b5d99bc8b910f90f72f80f20b8
Matheus Coelho <matheusvgp29@gmail.com>
2025-04-27 23:05:52 -0300

    fix: update import paths to remove './' prefix in HTML and JS files

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,8 +1,8 @@
-import "./displayBundles.js";
-import "./infiniteScroll.js";
-import "./darkmode.js";
-import "./modal18.js";
-import "./carousel.js";
-import "./buscador.js";
-import "./filtros.js";
-import "./footer.js";
+import "/displayBundles.js";
+import "/infiniteScroll.js";
+import "/darkmode.js";
+import "/modal18.js";
+import "/carousel.js";
+import "/buscador.js";
+import "/filtros.js";
+import "/footer.js";
